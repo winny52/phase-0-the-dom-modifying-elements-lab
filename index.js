312508b29037deb966e547bca5b016774a121ee2
WinnySigilai <winnysigilai02@gmail.com>
2023-06-20 11:09:56 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+
+const mainNode = document.querySelector("main#main");
+mainNode.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Emmanuel Mutisya is the champion";
